a4cc48a72f7e23b9f384d8511afbb84b690783df
coil398 <konayuta.sou@gmail.com>
2018-06-02 05:26:49 +0900

    post

--- mod_result_post.py
+++ mod_result_post.py
@@ -2,6 +2,7 @@ from account_helpers import BattleResultsCache
 from gui import SystemMessages
 from gui.SystemMessages import SM_TYPE
 from PlayerEvents import g_playerEvents
+import requests
 import json
 import copy
 
@@ -31,8 +32,23 @@ def _JSON_Encode(obj):
         return newList
     return obj
 
+
 def __onAccountBecomePlayer():
     SystemMessages.pushMessage('Hello World', type=SM_TYPE.GameGreeting)
+    post_to_discord()
+
+
+def post_to_discord():
+    url = 'https://discordapp.com/api/webhooks/452188196533436418/6AE7DIRLZ9VyNJ26z1VmikajVqO7ItQbr3wOKC2bLgMb_qjCeDexlSKb8E-rbzlZjcv_'
+    method = 'POST'
+    obj = {"username": "bot",
+              "content": "launched world of tanks."}
+    headers = {"Content-Type": "application/json"}
+    json_data = json.dumps(obj)
+
+    result = requests.post(url, data=json_data, headers=headers)
+    print(result.status_code)
+
 
 def init():
     g_playerEvents.onBattleResultsReceived += post_result_to_discord
